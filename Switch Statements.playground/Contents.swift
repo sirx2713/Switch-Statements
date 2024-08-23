a2c02eb65c597fe701e6cfc3089eb3abf9dbb277
iSAD <bradx2713@gmail.com>
2024-08-23 10:38:00 +0200

    Initial Commit

---
+++ Switch Statements.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+let myFavNumbers = 1
+
+switch myFavNumbers{
+case 0:
+    print("Having some fun!")
+case 1:
+    print("I love 6.")
+case 2:
+    print("I love 8.")
+case 3:
+    print("I love only these 2 numbers!")
+default:
+    print("Which numbers do I love?")
+}
